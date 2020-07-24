8d0e4040b45c7c78ee24c92dc9b50aab3c87e59f
Cathrine Kanini <catherinekanini001@gmail.com>
2020-07-24 15:35:42 +0300

    flashcard

---
+++ JS/scripts.js
@@ -0,0 +1,179 @@
+function toggleJavascript() {
+    let newContent = "JavaScript is the programming language of HTML and the Web.JavaScript is easy to learn.This tutorial will teach you JavaScript from basic to advanced"
+    let ourP = document.getElementById("java");
+    if (ourP.innerHTML === "Javascript") {
+        ourP.innerHTML = newContent
+    } else {
+        ourP.innerHTML = "Javascript"
+    }
+}
+function toggleOperators(){
+    let operator="An operator is a symbol that tells the compiler to perform specific mathematical or logical manipulations"
+    let ourP=document.getElementById("operators");
+    if(ourP.innerHTML==="Operators"){
+        ourP.innerHTML=operator
+    }
+    else{
+        ourP.innerHTML="Operators"
+    }
+}
+function toggleStore(){
+    let newContent="variable is a piece of memory that can contain a data value. A variable thus has a data type"
+    let ourP = document.getElementById("store");
+    if(ourP.innerHTML=== "Variables"){
+        ourP.innerHTML=newContent
+    }
+    else{
+        ourP.innerHTML="Variables"
+    }
+}
+function toggleConvection(){
+    let newContent="A naming convention is a convention (generally agreed scheme) for naming things."
+    let ourP=document.getElementById("nameRules")
+    if(ourP.innerHTML==="Variable naming convection"){
+        ourP.innerHTML=newContent
+    }
+    else{
+        ourP.innerHTML="Variable naming convection"
+    }
+}
+function toggleFuncc(){
+    let newContent="A function contains some code that will be executed by an event or a call to that function. A function is a set of statements"
+    let ourP=document.getElementById("func");
+    if(ourP.innerHTML==="Functions"){
+        ourP.innerHTML=newContent
+    }
+    else{
+        ourP.innerHTML="Functions"
+    }}
+function toggleString(){
+    let newContent="a string is traditionally a sequence of characters, either as a literal constant or as some kind of variable"
+    let ourP=document.getElementById("string")
+    if(ourP.innerHTML==="Strings"){
+        ourP.innerHTML=newContent
+    }else{
+        ourP.innerHTML="Strings"
+    }}
+    function toggleMetho(){
+        let newContent="A method in object-oriented programming (OOP) is a procedure associated with a message and an object"
+        let ourP=document.getElementById("method")
+        if(ourP.innerHTML==="Methods"){
+            ourP.innerHTML=newContent
+        }else{
+            ourP.innerHTML="Methods"
+        }}
+
+    function toggleArgum(){
+        let newContent="a parameter or a formal argument, is a special kind of variable, used in a subroutine to refer to one of the pieces of data provided as input to the subroutine."
+        let ourP=document.getElementById("argum")
+        if(ourP.innerHTML==="Arguments"){
+            urP.innerHTML=newContent
+            }
+            else{
+                ourP.innerHTML="Arguments"
+            }}
+
+    function toggleParameter(){
+        let newContent="Parameters allow us to pass information or instructions into functions and procedures . They are useful for numerical information such as stating the size of an object."
+        let ourP=document.getElementById("Parameters")
+        if(ourP.innerHTML==="Parameters"){
+            ourP.innerHTML=newContent
+            }
+            else{
+                ourP.innerHTML="Parameters"
+        }}
+
+    function toggleReturn(){
+        let newContent="a return statement causes execution to leave the current subroutine and resume at the point in the code immediately after the instruction which called the subroutine, known as its return address."
+        let ourP=document.getElementById("Return")
+        if(ourP.innerHTML==="Return"){
+            ourP.innerHTML=newContent
+
+        }else {
+            ourP.innerHTML="Return"
+        }
+    }
+    function toggleChain(){
+        let newContent=" is a common syntax for invoking multiple method calls in object-oriented programming languages"
+        let ourP=document.getElementById("Chaining methods")
+        if(ourP.innerHTML==="Chaining methods"){
+            ourP.innerHTML=newContent
+        }else{
+            ourP.innerHTML="Chaining methods"
+        } }
+    function toggleBool(){
+        let newContent="the Boolean data type is a data type that has one of two possible values (usually denoted true and false) which is intended to represent the two truth values of logic and Boolean algebra"
+        let ourP=document.getElementById("Boolean")
+        if(ourP.innerHTML==="Boolean"){
+            ourP.innerHTML=newContent
+        }else{
+            ourP.innerHTML="Boolean"
+ }}
+    function toggleUndef(){
+        let newContent="undefined value is a condition where an expression does not have a correct value, although it is syntactically correct"
+        let ourP=document.getElementById("Undefined")
+        if(ourP.innerHTML==="Undefined"){
+            ourP.innerHTML=newContent
+        }else{
+            ourP.innerHTML="Undefined"
+        }
+   }
+   function toggleNotANum(){
+       let newContent="not a number"
+       let ourP=document.getElementById("Nan")
+       if(ourP.innerHTML==="Nan"){
+           ourP.innerHTML=newContent
+       }else{
+           ourP.innerHTML="Nan"
+       }
+   }
+   function toggleEscape(){
+       let newContent=" an escape character is a character that invokes an alternative interpretation on subsequent characters in a character sequence. An escape character is a particular case of metacharacters"
+       let ourP=document.getElementById("Escape")
+       if(ourP.innerHTML==="Escape"){
+           ourP.innerHTML=newContent
+       }else{
+           ourP.innerHTML="Escape"
+       }
+   }
+   function toggleWarning(){
+       let newContent="gives a warning"
+       let ourP=document.getElementById("Alert")
+       if(ourP.innerHTML==="Alert"){
+           ourP.innerHTML=newContent
+       } else{
+           ourP.innerHTML="Alert"
+       }
+   }
+   function toggleCommen(){
+       let newContent="comments are  statements that the compiler will never execute one a program is run"
+       let ourP=document.getElementById("Comments")
+       if(ourP.innerHTML="Comments"){
+           ourP.innerHTML=newContent
+       }else{
+           ourP.innerHTML="Comments"
+       }
+   }
+   function toggleJquery(){
+       let newContent="jQuery is a lightweight, write less, do more."
+       let ourP=document.getElementById("JQuery")
+       if(ourP.innerHTML==="JQuery"){
+           ourP.innerHTML=newContent
+       }else{
+           ourP.innerHTML="JQuery"
+       }
+       }
+    function toggleAttribute(){
+        let newContent="an attribute is a specification that defines a property of an object, element, or file."
+        let ourP=document.getElementById("attributes")
+        if(ourP.innerHTML==="Attributes"){
+            ourP.innerHTML=newContent
+        }else{
+            ourP.innerHTML="Attributes"
+        }
+    }
+    
+    
+            
+
+
